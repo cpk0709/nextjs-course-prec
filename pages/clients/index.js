ee9cee13da3fdfde3703a4b4b7b31eeb9d5ffdfb
cpk0709 <cpk0709@gmail.com>
2022-09-28 16:10:31 +0900

    link 추가

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 const ClientPage = () => {
+  const clients = [
+    { id: "cpk", name: "Phil Choi" },
+    { id: "manu", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+        {/* <li>
+          <Link href="/clients/cpk">Phil Choi</Link>
+        </li>
+        <li>
+          <Link href="/clients/manu">Manuel</Link>
+        </li> */}
+      </ul>
     </div>
   );
 };
