932a6f62853c5b7afc609ef515fb5ab91d9975d5
cpk0709 <cpk0709@gmail.com>
2022-09-28 16:46:38 +0900

    add button and routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,11 +2,20 @@ import { useRouter } from "next/router";
 
 const ClientProjectsPage = () => {
   const router = useRouter();
-  console.log(router.pathname);
-  console.log(router.query);
+  // console.log(router.pathname);
+  // console.log(router.query);
+
+  const loadProjectHandler = () => {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: router.query, clientprojectid: "projeact1" },
+    });
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
