b525fdc5b03bcacf8a2730e1ee6795d83a890ae5
cpk0709 <cpk0709@gmail.com>
2022-09-28 15:08:18 +0900

    styled-components 리스트 적용

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import Link from "next/link";
+import styled from "styled-components";
+
 const HomePage = (props) => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ListUl>
+        <ListLi>
+          <Link href="/portfolio">Portfolio</Link>
+        </ListLi>
+        <ListLi>
+          <Link href="/clients">Clients</Link>
+        </ListLi>
+      </ListUl>
     </div>
   );
 };
 
 export default HomePage;
+
+const ListUl = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+const ListLi = styled.li`
+  padding: 20px;
+`;
