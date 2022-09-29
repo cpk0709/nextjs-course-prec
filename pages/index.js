c75cfd9295ba769c05f137f59f156a5a5ce1879a
cpk0709 <cpk0709@gmail.com>
2022-09-29 10:26:47 +0900

    edit homepage index

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,16 @@
 import Link from "next/link";
 import styled from "styled-components";
+import { getFeaturedEvents } from "../dummy-data";
 
 const HomePage = (props) => {
+  const featuredEvents = getFeaturedEvents();
+  console.log(featuredEvents);
+
   return (
     <div>
       <h1>The Home Page</h1>
-      <ListUl>
-        <ListLi>
-          <Link href="/portfolio">Portfolio</Link>
-        </ListLi>
-        <ListLi>
-          <Link href="/clients">Clients</Link>
-        </ListLi>
-      </ListUl>
     </div>
   );
 };
 
 export default HomePage;
-
-const ListUl = styled.ul`
-  display: flex;
-  list-style: none;
-`;
-
-const ListLi = styled.li`
-  padding: 20px;
-`;
