ca73c83dc4564baf74f794824dc040b349ff3f78
cpk0709 <cpk0709@gmail.com>
2022-09-29 17:35:32 +0900

    add event detail page and files

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,39 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const event = getEventById(router.query.eventId);
+
+  if (!event) {
+    return <h1>No event found!</h1>;
+  }
+
+  // const humanReadableDate = new Date(event.date).toLocaleDateString("ko-KR", {
+  //   year: "numeric",
+  //   month: "long",
+  //   day: "numeric",
+  // });
+
+  // const formattedAddress = event.location.replace(", ", "\n");
+
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <>
+      <EventSummary />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>event.description</p>
+      </EventContent>
+    </>
   );
 };
 
