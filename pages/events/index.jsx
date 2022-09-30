34b394b6a7cb927aa22a913953596d2ce48c42da
cpk0709 <cpk0709@gmail.com>
2022-09-30 17:19:24 +0900

    set 함수 props 전달

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,14 +1,17 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
+import { useState } from "react";
 
 const AllEventsPage = () => {
   const eventAll = getAllEvents();
 
+  const [eventList, setEventList] = useState(eventAll);
+
   return (
     <>
-      <EventSearch />
-      <EventList items={eventAll} />
+      <EventSearch setEventList={(eventArr) => setEventList(eventArr)} />
+      <EventList items={eventList} />
     </>
   );
 };
