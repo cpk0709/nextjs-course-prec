09f785e97113cdc187be5a9ffcf34c0a2c0a0a3f
cpk0709 <cpk0709@gmail.com>
2022-10-05 09:49:44 +0900

    delete state from index

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,17 +1,14 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
-import { useState } from "react";
 
 const AllEventsPage = () => {
   const eventAll = getAllEvents();
 
-  const [eventList, setEventList] = useState(eventAll);
-
   return (
     <>
       <EventSearch setEventList={(eventArr) => setEventList(eventArr)} />
-      <EventList items={eventList} />
+      <EventList items={eventAll} />
     </>
   );
 };
