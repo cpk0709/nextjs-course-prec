2e4b42b97270a68470c8d79fa070183caa5c5145
cpk0709 <cpk0709@gmail.com>
2022-10-05 10:01:42 +0900

    add findEventsHandler

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,21 @@
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
 
 const AllEventsPage = () => {
   const eventAll = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventSearch setEventList={(eventArr) => setEventList(eventArr)} />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={eventAll} />
     </>
   );
